08678f92115d97b2c550629a340925ac3df96016
alfseptyan <alfathseptyan37@gmail.com>
2024-12-29 07:25:55 +0700

    init

--- app/sentry-example-page/page.jsx
+++ app/sentry-example-page/page.jsx
@@ -41,7 +41,7 @@ export default function Page() {
           style={{
             padding: "12px",
             cursor: "pointer",
-            backgroundColor: "#AD6CAA",
+            backgroundColor: "##e6cc00",
             borderRadius: "4px",
             border: "none",
             color: "white",
